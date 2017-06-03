160d1d1b31b39032f7f34af7b590bfc02397a57a
tyrocjh <nashcjh@163.com>
2017-06-03 17:58:21 +0800

    tmp

--- README.md
+++ README.md
@@ -4,7 +4,14 @@
 2.使用user-agent
 3.限制并发量
 
-## technologies
+nodejs debug的使用:
+1.node debug task/collectProxy.js
+2.执行完1后程序会在第一行停止，输入c（continue），即可跳到断点处
+3.输入repl，放可调试变量
+4.crtl+c 退出debug模式
+
+
+## 技术
 
 * [superagent](http://visionmedia.github.io/superagent) 【实现主动发起get/post/delete等请求】
 * [cheerio](https://github.com/cheeriojs/cheerio) 【跟jquery几乎一样用法】
@@ -12,7 +19,7 @@
 * [async](https://github.com/caolan/async) 【控制并发的数量】
 
 
-## reference
+## 参考
 
 * [B站用户爬虫](https://github.com/airingursb/bilibili-user/blob/master/bilibili_user.py)
 * [爬虫-代理IP使用](http://www.cnblogs.com/hearzeus/p/5157016.html)
--- task/collectProxy.js
+++ task/collectProxy.js
@@ -1,58 +1,135 @@
+/*
+	功能：收集可用的代理IP，存放到/data下的proxy.txt
+*/
+
 var http = require("http"),
-	request = require('superagent')
-	cheerio = require("cheerio"),
-	async = require("async"),
-	eventproxy = require('eventproxy'),
-	jsonfile = require('jsonfile'),
-	_ = require('lodash');
+		request = require('superagent')
+		cheerio = require("cheerio"),
+		async = require("async"),
+		fs = require('fs'),
+		_ = require('lodash');
 
 require('superagent-proxy')(request);
 
-// var ep = new eventproxy();
-// 	mainUrl = 'http://mhbseal.com/', //入口页面
-// 	pageNum = 3, //要爬取文章的页数
-// 	pageUrls = [], //存放收集文章页面网站
-// 	startDate = new Date(),
-// 	endDate = false,
-// 	outputFile = 'task/data.json';
+var collectProxy = (function() {
+	// 存放收集到的代理IP
+	var proxyArr = [];
+	// 国内代理IP网站
+	var targetUrl = 'http://www.xicidaili.com/nn/';
+	// 测试代理IP是否可用
+	var proxyCheckUrl = 'http://ip.chinaz.com/getip.aspx';
 
-// function createData(M, arr) {
-// 	arr.forEach(function(obj) {
-// 		M.create(obj, function(err, model) {
-// 			if (err) {
-// 				console.info(err);
-// 			}
-// 		});
-// 	});
-// }
+	function writeData() {
+		var result = proxyArr.toString().replace(/,/g, '\n');
+		fs.writeFile('./task/data/proxy.txt', result, function(err) {
+		    if(err) {
+		    	console.log('写文件操作失败！');
+		    } else {
+		    	console.log('写文件操作成功！');
+		    }
+		});
+	}
 
-// function outputData(data) {
-// 	jsonfile.writeFile(outputFile, data, {
-// 		spaces: 2
-// 	}, function(err) {
-// 		if (err) {
-// 			console.info(err);
-// 		}
-// 	})
-// }
+	function checkData() {
+	 	request
+	  	.get(proxyCheckUrl)
+	  	.proxy('http://115.46.86.108:80')
+	  	.set({ 'User-Agent': 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 7.1; Trident/5.0)' })
+	  	.end(function(err, res){
+	  		if(err) {
+	  			console.log('error: ', err);
+	  		} else {
+	  			console.log('success...');
+	  			console.log(res.text);
+	  			// writeData();
+	  		}
+	  	});
 
-// function start() {
-// 	ep.after('ArticleJson', pageNum, function() {
-// 		outputData(articleArray);
-// 	})
+		// 并发控制
+    // async.mapLimit(urls, urls.length, function(url, callback) {
+    //     console.log("开始收集代理IP...");
+    //     requestAndCollect(url, callback);
+    // }, function(err, result) {
+    //     if(err) {
+    //       console.log('error: ', err);
+    //     } else {
+    //     	// console.log(result);
+    //       console.log("代理IP收集完毕！！！");
+    //       handleResult(result);
+    //       checkAndWrite();
+    //     }
+    // });
+	}
 
-// 	// 遍历文章列表页
-// 	pageUrls.forEach(function(pageUrl) {
-// 		request.get(pageUrl)
-// 			.end(function(err, res) {
-// 				articleArray = articleArray.concat(res.body.data.articles);
-// 				ep.emit('ArticleJson');
-// 			})
-// 	})
-// }
+	function handleResult(arr) {
+		for(var key in arr) {
+			proxyArr = proxyArr.concat(arr[key]);
+		}
+		proxyArr = _.uniq(proxyArr);
+	}
+
+	// 处理数组，并测试代理IP是否可用，最后写入本地文件
+	function checkAndWrite(result) {
+		handleResult(result);
+		console.info(proxyArr);
+		console.info(proxyArr.length);
+		// checkData();
+	}
+
+	function requestAndCollect(url, callback) {
+	 	request
+	  	.get(url)
+	  	.set({ 'User-Agent': 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 7.1; Trident/5.0)' })
+	  	.end(function(err, res) {
+	  		if(err) {
+	  			console.log('error: ', err);
+	  		} else {
+	  			var arr = [];
+	  			var $ = cheerio.load(res.text);
+	  			$('#ip_list tr').each(function(idx, el) {
+	  				if(idx !== 0) {
+		  				var ip = $(el).find('td').eq(1).text();
+		  				var port = $(el).find('td').eq(2).text();
+		  				var proxy = ip + ':' + port;
+		  				arr.push(proxy);
+	  				}
+	  			});
+	  			callback(null, arr);
+	  		}
+	  	});
+	}
+
+	// 收集代理IP
+	function collectData() {
+		var urls = [];
+
+		// 只取前5页的数据（每页100条数据）
+		for(var i=1; i<=2; i++) {
+			urls.push(targetUrl + i);
+		}
+
+		// 并发控制
+    async.mapLimit(urls, urls.length, function(url, callback) {
+        console.log("开始收集代理IP...");
+        requestAndCollect(url, callback);
+    }, function(err, result) {
+        if(err) {
+          console.log('error: ', err);
+        } else {
+        	// console.log(result);
+          console.log("代理IP收集完毕！！！");
+          checkAndWrite(result);
+        }
+    });
+	}
+
+	return {
+		collectData: collectData
+	}
+})();
 
 function start() {
-	
+	collectProxy.collectData();
 }
 
 start();
